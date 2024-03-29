8b00d4b0d5a62846d408fafe8ee58f907efd3eec
swhyeon98 <smreo75@gmail.com>
2024-03-29 23:40:06 +0900

    준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,11 +3,7 @@ package hellojpa;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.FlushModeType;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.TypedQuery;
-
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,29 +14,22 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+
         try {
             // 영속
-            Member memberA = new Member(300L, "memberA");
-            Member memberB = new Member(400L, "memberB");
-            Member memberC = new Member(500L, "memberC");
-            em.persist(memberA);
-            em.persist(memberB);
-            em.persist(memberC);
-            em.setFlushMode(FlushModeType.COMMIT);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            System.out.println("==========FLUSH==========");
-            TypedQuery<Member> query = em.createQuery("select m from Member as m", Member.class);
-            List<Member> members = query.getResultList();
-            for (Member member : members) {
-                System.out.println("member = " + member.getName());
-            }
+            em.clear();
+            System.out.println("====================");
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("====================");
             tx.commit();
         } catch (Exception e) {
             em.clear();
         }
-
         emf.close();
     }
 }
