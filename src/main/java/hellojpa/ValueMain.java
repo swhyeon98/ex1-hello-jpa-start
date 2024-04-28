a0b4597b3a591e56f4ca6a470067e28a6a7bd9d8
swhyeon98 <smreo75@gmail.com>
2024-04-28 23:46:48 +0900

    기본값 타입

---
+++ src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,13 @@
+package hellojpa;
+
+public class ValueMain {
+
+    public static void main(String[] args) {
+
+        Integer a = new Integer(10);
+        Integer b = a;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
